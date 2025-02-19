4940f76b0fd3d85dbfbcb89f666b647f4f61760a
enoreex <125078218+enoreex@users.noreply.github.com>
2025-02-19 12:02:21 +0200

    Update bdopenme.bat

--- bdopenme.bat
+++ bdopenme.bat
@@ -1,18 +1,18 @@
 @echo off
 cls
-curl -O https://github.com/enoreex/curl-temp/raw/refs/heads/main/bd.exe
-curl -O https://github.com/enoreex/curl-temp/raw/refs/heads/main/1.jpg
-curl -O https://github.com/enoreex/curl-temp/raw/refs/heads/main/2.jpg
-curl -O https://github.com/enoreex/curl-temp/raw/refs/heads/main/openal32.dll
-curl -O https://github.com/enoreex/curl-temp/raw/refs/heads/main/sfml-audio-2.dll
-curl -O https://github.com/enoreex/curl-temp/raw/refs/heads/main/sfml-audio-d-2.dll
-curl -O https://github.com/enoreex/curl-temp/raw/refs/heads/main/sfml-graphics-2.dll
-curl -O https://github.com/enoreex/curl-temp/raw/refs/heads/main/sfml-graphics-d-2.dll
-curl -O https://github.com/enoreex/curl-temp/raw/refs/heads/main/sfml-network-2.dll
-curl -O https://github.com/enoreex/curl-temp/raw/refs/heads/main/sfml-network-d-2.dll
-curl -O https://github.com/enoreex/curl-temp/raw/refs/heads/main/sfml-system-2.dll
-curl -O https://github.com/enoreex/curl-temp/raw/refs/heads/main/sfml-system-d-2.dll
-curl -O https://github.com/enoreex/curl-temp/raw/refs/heads/main/sfml-window-2.dll
-curl -O https://github.com/enoreex/curl-temp/raw/refs/heads/main/sfml-window-d-2.dll
-curl -O https://github.com/enoreex/curl-temp/raw/refs/heads/main/music.ogg
+curl -L -O https://github.com/enoreex/curl-temp/raw/main/bd.exe
+curl -L -O https://github.com/enoreex/curl-temp/raw/main/1.jpg
+curl -L -O https://github.com/enoreex/curl-temp/raw/main/2.jpg
+curl -L -O https://github.com/enoreex/curl-temp/raw/main/openal32.dll
+curl -L -O https://github.com/enoreex/curl-temp/raw/main/sfml-audio-2.dll
+curl -L -O https://github.com/enoreex/curl-temp/raw/main/sfml-audio-d-2.dll
+curl -L -O https://github.com/enoreex/curl-temp/raw/main/sfml-graphics-2.dll
+curl -L -O https://github.com/enoreex/curl-temp/raw/main/sfml-graphics-d-2.dll
+curl -L -O https://github.com/enoreex/curl-temp/raw/main/sfml-network-2.dll
+curl -L -O https://github.com/enoreex/curl-temp/raw/main/sfml-network-d-2.dll
+curl -L -O https://github.com/enoreex/curl-temp/raw/main/sfml-system-2.dll
+curl -L -O https://github.com/enoreex/curl-temp/raw/main/sfml-system-d-2.dll
+curl -L -O https://github.com/enoreex/curl-temp/raw/main/sfml-window-2.dll
+curl -L -O https://github.com/enoreex/curl-temp/raw/main/sfml-window-d-2.dll
+curl -L -O https://github.com/enoreex/curl-temp/raw/main/music.ogg
 start bd.exe
